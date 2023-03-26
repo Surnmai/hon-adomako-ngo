0fc60007df1cbbba5e710f11daa4f3917445c41a
kingsley-nmai <kingsley.nmai@amalitech.org>
2023-03-26 12:57:19 +0000

    update ok.js

--- src/Pages/Ok.js
+++ src/Pages/Ok.js
@@ -1,12 +1,12 @@
 import React from "react";
-import About from "../Components/Mission";
-import Hero from "../Components/Hero";
+// import About from "../Components/Mission";
+// import Hero from "../Components/Hero";
 
 const Ok = () => {
   return (
     <>
       <h4>Hello</h4>
-      <About />
+      {/* <About /> */}
     </>
   );
 };
