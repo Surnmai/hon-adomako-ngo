e803d36ce7202afbac40c9e8d3f4c9052ab42e07
Surnmai <surnmai1@gmail.com>
2023-04-24 14:49:15 +0000

    noon update

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -72,7 +72,7 @@ const ContactForm = () => {
           <div className="inputField">
             <input
               type="tel "
-              placeholder="(+233) 0244039294"
+              placeholder="(+233) 0200757774"
               className="inputText"
               required
               name="number"
