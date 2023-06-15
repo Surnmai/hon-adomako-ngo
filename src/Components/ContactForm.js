3f7f3628b8f0a0745b0602c390a49999ba023e3c
Surnmai <surnmai1@gmail.com>
2023-06-15 14:30:27 +0000

    map re-update

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -110,11 +110,19 @@ const ContactForm = () => {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3971.0740782144985!2d-0.2031113249387216!3d5.556037733649162!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf909be3522d8d%3A0x83ecc364da8102c4!2sWorld%20Trade%20Center%2C%20Accra!5e0!3m2!1sen!2sgh!4v1680692130939!5m2!1sen!2sgh"
         loading="lazy"
       ></iframe> */}
-      <iframe
+      {/* <iframe
         title="map"
         className="map"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d588.5354854278264!2d-0.2580362059122439!3d5.603855641899691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9900a6c2453f%3A0xef3ab82d9b2175ff!2sTimeless%20Links%20Restaurant!5e1!3m2!1sen!2sgh!4v1686839102516!5m2!1sen!2sgh"
         loading="lazy"
+      ></iframe> */}
+
+      <iframe
+        title="map"
+        className="map"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1758.4928244551868!2d-0.25841739228872873!3d5.60449512051223!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9900a6c2453f%3A0xef3ab82d9b2175ff!2sTimeless%20Links%20Restaurant!5e0!3m2!1sen!2sgh!4v1686839324341!5m2!1sen!2sgh"
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
       ></iframe>
     </>
   );
