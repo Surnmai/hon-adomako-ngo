1bea21c9dd416981c5bc20462f47b496d43eca9e
Surnmai <surnmai1@gmail.com>
2023-06-21 18:32:12 +0000

    update on about card

--- src/Components/Global/BulletContent.js
+++ src/Components/Global/BulletContent.js
@@ -11,15 +11,19 @@ const BulletContent = ({
 }) => {
   return (
     <>
-      <h3>{bold}</h3>
-      <article>{article}</article>
-      <ol>
-        <li>{text1}</li>
-        <li>{text2}</li>
-        <li>{text3}</li>
-        <li>{text4}</li>
-        <li>{text5}</li>
-      </ol>
+      <section>
+        <div className="about-us-card">
+          <h3>{bold}</h3>
+          <article>{article}</article>
+          <ol>
+            <li>{text1}</li>
+            <li>{text2}</li>
+            <li>{text3}</li>
+            <li>{text4}</li>
+            <li>{text5}</li>
+          </ol>
+        </div>
+      </section>
     </>
   );
 };
--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -5,7 +5,7 @@ import About from "../images/about.jpg";
 
 // import components
 import TopBanner from "../Components/Global/TopBanner";
-import ObjectiveCard from "../Components/Global/ObjectiveCard";
+// import ObjectiveCard from "../Components/Global/ObjectiveCard";
 import BulletContent from "../Components/Global/BulletContent";
 import OurTeam from "../Components/OurTeam";
 import DataWithTittle from "../Components/Global/DataWithTittle";
@@ -21,11 +21,11 @@ const AboutUs = () => {
         {textWithTittle.map((content, index) => {
           return <DataWithTittle key={index} {...content} />;
         })}
-
-        {bulletText.map((bullets, index) => {
-          return <BulletContent key={index} {...bullets} />;
-        })}
       </section>
+
+      {bulletText.map((bullets, index) => {
+        return <BulletContent key={index} {...bullets} />;
+      })}
       {/* <Content
         // title="AIPERD is an African-managed and African-owned non-governmental, non-religious, development-focused organization, with the aim to enhance Africa’s socio-economic development, through evidence-based policy making and capacity building. We are committed to supporting evidence-driven research"
         text="The African Institute for Policy Evaluation Research and Development (AIPERD) is an independent non-profit think tank based in Ghana. It focuses on policy advice, advocacy, and training to build capacity in developing countries, relying on cutting-edge research. AIPERD develops partnerships and linkages with research organizations and development partners, conducts research, provides professional and competent policy analysis, advice, and advocacy to enhance capacity building in the areas of education, health, environment, entrepreneurship, and governance. Additionally, the institution offers advisory services and expertise in macroeconomic fiscal policy management. Its team of experts possesses diverse competencies spanning business, finance, banking, economics, public policy, and law."
