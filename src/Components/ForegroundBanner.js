8b42895e477aa40448e2124b5b09575977870f1d
Surnmai <surnmai1@gmail.com>
2023-07-11 15:48:31 +0000

    Foreground edit

--- src/Components/ForegroundBanner.js
+++ src/Components/ForegroundBanner.js
@@ -4,7 +4,9 @@ import React from "react";
 import Button from "./Global/Button";
 
 // import icons
-import { FaAward, FaHandsHelping, FaProjectDiagram } from "react-icons/fa";
+import { FaHandsHelping } from "react-icons/fa";
+import { GiHazardSign } from "react-icons/gi";
+import { IoSchool } from "react-icons/io5";
 
 const ForegroundBanner = () => {
   return (
@@ -12,32 +14,34 @@ const ForegroundBanner = () => {
       <section className="frgBackground">
         <div className="content">
           <h3 className="title">
-            We have contributed to self developing projects for the past 20
-            years
+            {/* We have contributed to self developing projects for the past 20
+            years */}
+            AIPERD researches Ghana's Free SHS, African political change, and
+            environmental degradation policies.
           </h3>
           <Button name="Become part" path="/contact" />
         </div>
         <div className="icons">
           <div className="container">
             <div className="box">
-              <FaAward />
+              <IoSchool />
               <div className="content">
-                <h3>365</h3>
-                <small> awards</small>
+                {/* <h3>365</h3> */}
+                <small> Free SHS</small>
               </div>
             </div>
             <div className="box">
               <FaHandsHelping />
               <div className="content">
-                <h3>365</h3>
-                <small>partners</small>
+                {/* <h3>365</h3> */}
+                <small>Political Change</small>
               </div>
             </div>
             <div className="box">
-              <FaProjectDiagram />
+              <GiHazardSign />
               <div className="content">
-                <h3>365</h3>
-                <small>projects</small>
+                {/* <h3>365</h3> */}
+                <small>Environmental Degradation</small>
               </div>
             </div>
           </div>
