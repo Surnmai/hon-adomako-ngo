6c49bc3bfdc3db3928a2c5c64d29a43d96a31621
Surnmai <surnmai1@gmail.com>
2023-12-01 07:05:16 +0000

    article added

--- src/Pages/Readmore.js
+++ src/Pages/Readmore.js
@@ -13,8 +13,21 @@ const Readmore = () => {
   const { id } = useParams();
   const readMore = recentProjects.find((recent) => recent.id === id);
 
-  const { text, detailedText, image, title } = readMore;
-
+  const { text, detailedText, image, title, more } = readMore;
+  const {
+    title1,
+    title2,
+    title3,
+    title4,
+    title5,
+    title6,
+    content1,
+    content2,
+    content3,
+    content4,
+    content5,
+    content6,
+  } = more;
   return (
     <>
       <Helmet>
@@ -22,10 +35,30 @@ const Readmore = () => {
         <meta name="description" content={text} />
       </Helmet>
       <section className="readMore">
-        <img src={image} alt={title} />
+        <div className="img">
+          <img src={image} alt={title} />
+        </div>
         <h1>{title}</h1>
         <article className="summary">{text}</article>
         <article>{detailedText}</article>
+
+        <h1>{title1}</h1>
+        <article>{content1}</article>
+
+        <h1>{title2}</h1>
+        <article>{content2}</article>
+
+        <h1>{title3}</h1>
+        <article>{content3}</article>
+
+        <h1>{title4}</h1>
+        <article>{content4}</article>
+
+        <h1>{title5}</h1>
+        <article>{content5}</article>
+
+        <h1>{title6}</h1>
+        <article>{content6}</article>
       </section>
     </>
   );
